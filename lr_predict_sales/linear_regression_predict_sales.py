27ba54b145a39228b7139c81a980048b37684bfc
doyager <nayanws@gmail.com>
2018-11-13 14:31:16 -0500

    Update linear_regression_predict_sales.py
    added model with all parameters

--- lr_predict_sales/linear_regression_predict_sales.py
+++ lr_predict_sales/linear_regression_predict_sales.py
@@ -103,3 +103,78 @@ coeff['Coefficient Estimate'] = Series(lreg.coef_)
 calculating r-square
 
 lreg.score(x_cv,y_cv) 0.32942
+
+
+
+## case 3 , with all parameters
+
+Data pre-processing steps for regression model
+# imputing missing values
+
+train['Item_Visibility'] = train['Item_Visibility'].replace(0,np.mean(train['Item_Visibility']))
+
+train['Outlet_Establishment_Year'] = 2013 - train['Outlet_Establishment_Year']
+
+train['Outlet_Size'].fillna('Small',inplace=True)
+
+# creating dummy variables to convert categorical into numeric values
+
+mylist = list(train1.select_dtypes(include=['object']).columns)
+
+dummies = pd.get_dummies(train[mylist], prefix= mylist)
+
+train.drop(mylist, axis=1, inplace = True)
+
+X = pd.concat([train,dummies], axis =1 )
+
+Building the model
+import numpy as np
+
+import pandas as pd
+
+from pandas import Series, DataFrame
+
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+
+train = pd.read_csv('training.csv')
+
+test = pd.read_csv('testing.csv')
+
+# importing linear regression
+
+from sklearn from sklearn.linear_model import LinearRegression
+
+lreg = LinearRegression()
+
+# for cross validation
+
+from sklearn.model_selection import train_test_split
+
+X = train.drop('Item_Outlet_Sales',1)
+
+x_train, x_cv, y_train, y_cv = train_test_split(X,train.Item_Outlet_Sales, test_size =0.3)
+
+# training a linear regression model on train
+
+lreg.fit(x_train,y_train)
+
+# predicting on cv
+
+pred_cv = lreg.predict(x_cv)
+
+# calculating mse
+
+mse = np.mean((pred_cv - y_cv)**2)
+
+mse
+
+1348171.96
+
+# evaluation using r-square
+
+lreg.score(x_cv,y_cv)
+
+0.54831541460870059
+
