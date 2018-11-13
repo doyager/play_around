64bbc3dbbcc6718bb119f16b6f2bb3bd89899610
doyager <nayanws@gmail.com>
2018-11-13 14:20:42 -0500

    Update linear_regression_predict_sales.py
    model with more parameters

--- linear_regression_predict_sales.py
+++ linear_regression_predict_sales.py
@@ -49,3 +49,57 @@ coeff = DataFrame(x_train.columns)
 coeff['Coefficient Estimate'] = Series(lreg.coef_)
 
 coeff
+
+r-square for the above model.
+
+lreg.score(x_cv,y_cv)
+
+# more variables
+
+X = train.loc[:,['Outlet_Establishment_Year','Item_MRP','Item_Weight']]
+
+splitting into training and cv for cross validation
+
+x_train, x_cv, y_train, y_cv = train_test_split(X,train.Item_Outlet_Sales)
+
+## training the model
+
+lreg.fit(x_train,y_train)
+
+ValueError: Input contains NaN, infinity or a value too large for dtype(‘float64’).
+
+It produces an error, because item weights column have some missing values. So let us impute it with the mean of other non-null entries.
+
+train['Item_Weight'].fillna((train['Item_Weight'].mean()), inplace=True)
+
+Let us try to run the model again.
+
+training the model lreg.fit(x_train,y_train)
+
+## splitting into training and cv for cross validation
+
+x_train, x_cv, y_train, y_cv = train_test_split(X,train.Item_Outlet_Sales)
+
+## training the model lreg.fit(x_train,y_train)
+
+predicting on cv pred = lreg.predict(x_cv)
+
+calculating mse
+
+mse = np.mean((pred - y_cv)**2)
+
+mse
+
+1853431.59
+
+## calculating coefficients
+
+coeff = DataFrame(x_train.columns)
+
+coeff['Coefficient Estimate'] = Series(lreg.coef_)
+
+
+
+calculating r-square
+
+lreg.score(x_cv,y_cv) 0.32942
