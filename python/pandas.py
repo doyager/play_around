205c5093fdb8073056ac3d3f322523c57c7e916f
doyager <nayanws@gmail.com>
2018-11-29 13:43:47 -0500

    Create pandas.py
    initial commit

---
+++ python/pandas.py
@@ -0,0 +1,113 @@
+#python tutorials:
+
+python3 -c 'print("hello world")'
+
+#range
+>>> range(5)
+range(0, 5)
+>>>
+>>> list(range(5)) # list() call is not required in Python 2
+[0, 1, 2, 3, 4]
+
+#create df with out index_col
+import pandas as pd
+data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]}
+df = pd.DataFrame(data)
+print (df)
+
+#create df with index
+
+import pandas as pd
+data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky'],'Age':[28,34,29,42]}
+df = pd.DataFrame(data, index=['rank1','rank2','rank3','rank4'])
+print (df)
+>>> print (df)
+   Age   Name
+0   28    Tom
+1   34   Jack
+2   29  Steve
+3   42  Ricky
+>>> df.shape  #gives (rows,cols)
+(4, 2)
+>>>
+>>> df.shape[0] #gives only no of rows
+4
+
+
+#drop drop_duplicates
+import pandas as pd
+data = {'Name':['Tom', 'Jack', 'Steve', 'Ricky', 'Ricky'],'Age':[28,34,29,42,42]}
+df = pd.DataFrame(data)
+>>> print(df)
+   Age   Name
+0   28    Tom
+1   34   Jack
+2   29  Steve
+3   42  Ricky
+4   42  Ricky
+>>> df1 = df['Name'].drop_duplicates()
+>>> print(df1)
+0      Tom
+1     Jack
+2    Steve
+3    Ricky
+Name: Name, dtype: object
+
+# pandas - data frame merge or joins
+
+#join on single col
+import pandas as pd
+left = pd.DataFrame({
+         'id':[1,2,3,4,5],
+         'Name': ['Alex', 'Amy', 'Allen', 'Alice', 'Ayoung'],
+         'subject_id':['sub1','sub2','sub4','sub6','sub5']})
+right = pd.DataFrame(
+         {'id':[1,2,3,4,5],
+         'Name': ['Billy', 'Brian', 'Bran', 'Bryce', 'Betty'],
+         'subject_id':['sub2','sub4','sub3','sub6','sub5']})
+print pd.merge(left,right,on='id')
+
+   Name_x  id_x   subject_id   Name_y   id_y
+0     Alex   1.0         sub1      NaN    NaN
+1      Amy   2.0         sub2    Billy    1.0
+2    Allen   3.0         sub4    Brian    2.0
+3    Alice   4.0         sub6    Bryce    4.0
+4   Ayoung   5.0         sub5    Betty    5.0
+5      NaN   NaN         sub3     Bran    3.0
+
+#join on multiple col
+import pandas as pd
+left = pd.DataFrame({
+         'id':[1,2,3,4,5],
+         'Name': ['Alex', 'Amy', 'Allen', 'Alice', 'Ayoung'],
+         'subject_id':['sub1','sub2','sub4','sub6','sub5']})
+right = pd.DataFrame(
+         {'id':[1,2,3,4,5],
+         'Name': ['Billy', 'Brian', 'Bran', 'Bryce', 'Betty'],
+         'subject_id':['sub2','sub4','sub3','sub6','sub5']})
+print pd.merge(left,right,on=['id','subject_id'])
+
+    Name_x   id_x   subject_id   Name_y   id_y
+0      Amy      2         sub2    Billy      1
+1    Allen      3         sub4    Brian      2
+2    Alice      4         sub6    Bryce      4
+3   Ayoung      5         sub5    Betty      5
+
+
+
+
+#connect DB - odbc
+import pyodbc
+import os
+import pandas as pd
+
+uid = ""
+pwd = ""
+dbcname = "DWPROD1COP1.CORP.ANTHEM.COM"
+odbc_string = "DRIVER={Teradata};DBCNAME=" + dbcname + ";UID=" + uid + ";PWD=" + pwd + ";QUIETMODE=YES;Authentication=LDAP;"
+conn = pyodbc.connect(odbc_string)
+query = """select * from test"""
+raw_data = pd.read_sql(query, conn)
+print(raw_data)
+row_dict = raw[key].drop_duplicates()  #drop d
+n_claims = row_dict.shape[0]
