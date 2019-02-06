1521232bea41cfa500ccd43a0d46b37e19a1f194
doyager <nayanws@gmail.com>
2019-02-06 15:37:58 -0500

    Update plotting.py
    comments

--- python/plotting.py
+++ python/plotting.py
@@ -6,3 +6,9 @@ plt.plot([1,2,3,4])
 plt.xlabel('Progamming Languages')
 plt.ylabel('Counts')
 plt.show()
+'''
+You may be wondering why the x-axis ranges from 0-3 and the y-axis from 1-4. If you provide a single
+list or array to the plot() command, matplotlib assumes it is a sequence of y values, and automatically
+generates the x values for you. Since python ranges start with 0, the default x vector has the same length
+as y but starts with 0. Hence the x data are [0,1,2,3].
+'''
