2f576215d62800050da1cdb61655f530971fd1ff
doyager <nayanws@gmail.com>
2019-02-14 13:37:51 -0500

    Update matplotlib.py
    pie chart

--- python/matplotlib.py
+++ python/matplotlib.py
@@ -20,3 +20,26 @@ axes1.plot(x,y)
 axes2.plot(y,x)
 
 plt.show()
+
+
+
+
+
+
+
+# pie chart
+
+import matplotlib.pyplot as plt
+ 
+# Data to plot
+labels = 'Python', 'C++', 'Ruby', 'Java'
+sizes = [215, 130, 245, 210]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0, 0, 0)  # explode 1st slice
+ 
+# Plot
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+autopct='%1.1f%%', shadow=True, startangle=140)
+ 
+plt.axis('equal')
+plt.show()
