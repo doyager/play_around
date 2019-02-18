75e2498b4917b5abc7d1aa2f0eef3cb04d271590
doyager <nayanws@gmail.com>
2019-02-18 15:19:15 -0500

    Create basic_command.sh
    initial commit

---
+++ linux/basic_command.sh
@@ -0,0 +1,20 @@
+
+
+
+
+
+# disk space 
+
+  #at parent dir level , -s sums up
+  du -skh /home/mac
+  
+  # to give all sub dir and files under a path
+  du -kh
+  
+  
+
+#vi 
+
+# find and replace
+
+:%s/search_string/replacement_string/g
