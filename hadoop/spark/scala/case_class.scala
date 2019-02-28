ea955443defe006816260bb6dee451d68011621c
doyager <nayanws@gmail.com>
2019-02-28 16:33:26 -0500

    Update case_class.scala
    case class

--- hadoop/spark/scala/case_class.scala
+++ hadoop/spark/scala/case_class.scala
@@ -1,9 +1,18 @@
 
+# syntax
+
+%scala
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.catalyst.ScalaReflection
+
+case class A(key: String, time: java.sql.Timestamp, date: java.sql.Date, decimal: java.math.BigDecimal, map: Map[String, Int], nested: Seq[Map[String, Seq[Int]]])
+val schema = ScalaReflection.schemaFor[A].dataType.asInstanceOf[StructType]
+schema.printTreeString
 
 
 #########################################
 ## Case class
-
+# Link : http://www.infoobjects.com/2016/06/21/spark-inferring-schema-using-case-classes/
 Recipe for this is given below
 
 1. Start the spark shell and give it some additional memory:
@@ -45,3 +54,136 @@ $ hdfs dfs -put person person
  scala> people.collect.foreach (printIn)
  
  #########################################
+
+#### case class 2  with triats addition
+#link : https://blog.codecentric.de/en/2016/07/spark-2-0-datasets-case-classes/
+
+
+val df: DataFrame = spark.read
+                         .schema(schema)
+                         .option("header", true)
+                         .csv("/path/to/bodies.csv")
+
+val id       = StructField("id",       DataTypes.IntegerType)
+val width    = StructField("width",    DataTypes.DoubleType)
+val height   = StructField("height",   DataTypes.DoubleType)
+val depth    = StructField("depth",    DataTypes.DoubleType)
+val material = StructField("material", DataTypes.StringType)
+val color    = StructField("color",    DataTypes.StringType)
+ 
+val fields = Array(id, width, height, depth, material, color)
+val schema = StructType(fields)
+
+#Spark 2.0 introduces Datasets to better address these points. The take away message is that instead of using type agnostic Rows,
+#one can use Scala’s case classes or tuples to describe the contents of the rows. The (not so) magic gluing is done by using as on 
+#a Dataframe. (Tupels would match by position and also lack the possibility to customize naming.)
+
+final case class Body(id: Int, 
+                      width: Double, 
+                      height: Double, 
+                      depth: Double, 
+                      material: String, 
+                      color: String)
+ 
+val ds = df.as[Body]
+The matching between the DataFrames columns and the fields of the case class is done by name and the types should match. In summary, this introduces a contract and narrows down possible sources of error. For example, one immediate benefit is that we can access fields via the dot operator and get additional IDE support:
+
+val colors = ds.map(_.color) // Compiles!
+ds.map(_.colour)             // Typo - WON'T compile!
+Further, we can use this feature and the newly added type-safe aggregation functions to write queries with compile time safety:
+
+import org.apache.spark.sql.expressions.scalalang.typed.{
+  count => typedCount, 
+  sum => typedSum}
+ 
+ds.groupByKey(body => body.color)
+  .agg(typedCount[Body](_.id).name("count(id)"),
+       typedSum[Body](_.width).name("sum(width)"),
+       typedSum[Body](_.height).name("sum(height)"),
+       typedSum[Body](_.depth).name("sum(depth)"))
+  .withColumnRenamed("value", "group")
+  .alias("Summary by color level")
+  .show()
+If we wanted to compute the volume of all bodies, this would be quite straightforward in the DataFrame API. Two solutions come to mind:
+
+// 1. Solution: Using a user-defined function and appending the results as column
+val volumeUDF = udf {
+ (width: Double, height: Double, depth: Double) => width * height * depth
+}
+ 
+ds.withColumn("volume", volumeUDF($"width", $"height", $"depth"))
+ 
+// 2. Solution: Using a SQL query
+spark.sql(s"""
+           |SELECT *, width * height * depth
+           |AS volume
+           |FROM bodies
+           |""".stripMargin)
+
+
+
+# udf 
+// 1. Solution: Using a user-defined function and appending the results as column
+val volumeUDF = udf {
+ (width: Double, height: Double, depth: Double) => width * height * depth
+}
+ 
+ds.withColumn("volume", volumeUDF($"width", $"height", $"depth"))
+ 
+// 2. Solution: Using a SQL query
+spark.sql(s"""
+           |SELECT *, width * height * depth
+           |AS volume
+           |FROM bodies
+           |""".stripMargin)
+
+
+
+ds.map { 
+ body => 
+  val volume = body.width * body.height * body.depth
+  BodyWithVolume(body.id, body.width, body.height, body.depth, body.material, body.color, volume)
+}
+
+
+# traits 
+trait IsIdentifiable {
+ def id: Int
+}
+ 
+trait HasThreeDimensions {
+ def width: Double
+ def height: Double
+ def depth: Double
+}
+ 
+trait ConsistsOfMaterial {
+ def material: String
+ def color: String
+}
+ 
+trait HasVolume extends HasThreeDimensions {
+ def volume = width * height * depth
+}
+ 
+final case class Body(id: Int, 
+                      width: Double, 
+                      height: Double, 
+                      depth: Double, 
+                      material: String, 
+                      color: String) extends 
+                      IsIdentifiable with 
+                      HasThreeDimensions with 
+                      ConsistsOfMaterial
+ 
+final case class BodyWithVolume(id: Int, 
+                                width: Double, 
+                                height: Double, 
+                                depth: Double, 
+                                material: String, 
+                                color: String) extends 
+                                IsIdentifiable with 
+                                HasVolume with 
+                                ConsistsOfMaterial
+
+
