834bf3c32b4bb1e74d3108536d3f62fdc1480b80
doyager <nayanws@gmail.com>
2019-03-01 10:29:48 -0500

    Update misc_snippets.py
    age udf

--- python/misc_snippets.py
+++ python/misc_snippets.py
@@ -1,6 +1,40 @@
 
+#####################################
+# .            UDF
+#####################################
 
 
+# calculate age from str date , age udf 
+        from datetime import datetime
+        from datetime import date
+
+        #YYYY-MM-DDTHH:MM:SS+HH:MM
+        # "%Y-%m-%d"
+        # input : 1978-02-13 00:00:00.0     output : 41
+        def calculate_age(born):
+            #born = datetime.strptime(born, "YYYY-MM-DD HH:MM:SS.s").date()
+            born = datetime.strptime(born, "%Y-%m-%d %H:%M:%S.%f").date()
+            today = date.today()
+            return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+
+        input_1M['age'] = input_1M['member_birth_date'].apply(calculate_age)
+        
+        input_1M[['member_birth_date','age']].head(5)
+        
+        """"
+         o/p:
+         
+        0       1978-02-13 00:00:00.0   41
+        1       1978-02-13 00:00:00.0   41
+        2       1957-01-24 00:00:00.0   62
+        3       1969-04-13 00:00:00.0   49
+        4       1992-03-13 00:00:00.0   26
+        """
+
+
+#####################################
+# .            ERRORS
+#####################################
 #installation error , install
 
         #ssl error
