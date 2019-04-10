2fb6e4e45b1daffdb298ba832c01679e9566c609
doyager <nayanws@gmail.com>
2019-04-10 12:16:58 -0400

    Update basics.scala
    create data frame :

--- hadoop/spark/scala/basics.scala
+++ hadoop/spark/scala/basics.scala
@@ -1,3 +1,55 @@
+#create data frame :
+
+
+#method 1:
+
+
+        val someData = Seq(
+          Row(8, "bat"),
+          Row(64, "mouse"),
+          Row(-27, "horse")
+        )
+
+        val someSchema = List(
+          StructField("number", IntegerType, true),
+          StructField("word", StringType, true)
+        )
+
+        val someDF = spark.createDataFrame(
+          spark.sparkContext.parallelize(someData),
+          StructType(someSchema)
+        )
+
+#method 2:
+
+
+        //The toDF() method can be called on a sequence object to create a DataFrame.
+      import spark.implicits._
+      
+
+      val someDF = Seq(
+        (8, "bat"),
+        (64, "mouse"),
+        (-27, "horse")
+      ).toDF("number", "word")
+
+
+#method 3:
+
+      createDF() is defined in spark-daria and allows for the following terse syntax.
+
+      val someDF = spark.createDF(
+        List(
+          (8, "bat"),
+          (64, "mouse"),
+          (-27, "horse")
+        ), List(
+          ("number", IntegerType, true),
+          ("word", StringType, true)
+        )
+      )
+
+
 
 #udf - spark sql
 
