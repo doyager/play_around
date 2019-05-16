52f33f749fc983c3d0f7ae2afb159cf720995c7c
doyager <nayanws@gmail.com>
2019-05-16 11:10:19 -0400

    Create pyspark_basic_standalone.py
    basic create dataframe

---
+++ hadoop/spark/pyspark/pyspark_basic_standalone.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from pyspark import SparkConf, SQLContext
+from pyspark.sql import SparkSession
+
+"""
+ create dataframe and print it
+ spark submit command :   spark2-submit ./pyspark_basic_standalone.py
+"""
+
+# Initializing spark session & Hive Context
+print (">>>>Spark: Initializing SparkSession ..")
+spark = SparkSession.builder.appName("basic_standalone_app").getOrCreate()
+df = spark.createDataFrame([("danny",1),("jon",2),("sam",3)], ['name','id'])
+df.show()
+
+
+
+"""
+output :
++-----+---+
+| name| id|
++-----+---+
+|danny|  1|
+|  jon|  2|
+|  sam|  3|
++-----+---+
+"""
