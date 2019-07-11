1ed47c95c960d460e6f8bf09bd0aff5cdd9006a5
doyager <nayanws@gmail.com>
2019-07-11 13:24:46 -0400

    Update misc_queries.sql
    -- regexp_like

--- sql/misc_queries.sql
+++ sql/misc_queries.sql
@@ -5,3 +5,9 @@
 
 select  marks_id from MARKS where student_id = 102 AND (CREATED_TIMESTAMP  BETWEEN to_timestamp('2019-07-11 00:00:00', 'yyyy-mm-dd hh24:mi:ss')
  AND to_timestamp('2019-07-11 23:59:59', 'yyyy-mm-dd hh24:mi:ss')))
+
+
+-- combination of LIKE and IN
+-- regexp_like
+select  emp_id from employee where  regexp_like(NAME,'mac-name1|mac-name2')             
+                  
