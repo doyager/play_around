23802f7e8577c8dfe0035bf1da76f4453751d26b
doyager <nayanws@gmail.com>
2019-10-22 18:54:22 -0400

    Update scala_basics.scala
    flatmap vs map

--- hadoop/spark/scala/scala_basics.scala
+++ hadoop/spark/scala/scala_basics.scala
@@ -1,3 +1,44 @@
+#map vs flat map
+Quite a difference, right? Because of the way flatMap works, 
+it flattens the resulting list of strings into a sequence of characters (Seq[Char]). 
+I like to think of flatMap as a combination of map followed by flatten, so it first
+
+Tip: I often refer to flatMap as mapFlat so I can remember how it works.
+
+scala> val fruits = Seq("apple", "banana", "orange")
+fruits: Seq[java.lang.String] = List(apple, banana, orange)
+
+scala> fruits.map(_.toUpperCase)
+res0: Seq[java.lang.String] = List(APPLE, BANANA, ORANGE)
+
+scala> fruits.flatMap(_.toUpperCase)
+res1: Seq[Char] = List(A, P, P, L, E, B, A, N, A, N, A, O, R, A, N, G, E)
+
+
+(or)
+
+scala> val mapResult = fruits.map(_.toUpperCase)
+mapResult: Seq[String] = List(APPLE, BANANA, ORANGE)
+
+scala> val flattenResult = mapResult.flatten
+flattenResult: Seq[Char] = List(A, P, P, L, E, B, A, N, A, N, A, O, R, A, N, G, E)
+
+
+# examnple flatMap and sum
+
+scala> val strings = Seq("1", "2", "foo", "3", "bar")
+strings: Seq[java.lang.String] = List(1, 2, foo, 3, bar)
+
+scala> strings.map(toInt)
+res0: Seq[Option[Int]] = List(Some(1), Some(2), None, Some(3), None)
+
+scala> strings.flatMap(toInt)
+res1: Seq[Int] = List(1, 2, 3)
+
+scala> strings.flatMap(toInt).sum
+res2: Int = 6
+
+
 #sum list 
 val l = List(1, 3, 5, 11, -1, -3, -5)
 l.foldLeft(0)(_ + _) // same as l.foldLeft(0)((a,b) => a + b)
@@ -11,6 +52,9 @@ lol: List[List[Int]] = List(List(1, 2), List(3, 4))
 val result = lol.flatten
 result: List[Int] = List(1, 2, 3, 4)
 
+(or)
+List(List(1,2), List(3,4)).flatten
+> List(1,2,3,4)
 
 
 #List
