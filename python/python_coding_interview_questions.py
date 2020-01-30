38c39903c2efbcc988b554e54bca0b6163638a44
doyager <nayanws@gmail.com>
2020-01-29 19:07:23 -0500

    Update python_coding_interview_questions.py
    two sum

--- python/python_coding_interview_questions.py
+++ python/python_coding_interview_questions.py
@@ -1,4 +1,25 @@
 
+
+
+
+
+
+
+12. two sum
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################
 which are divisible by 7 but are not a multiple of 5
 l=[]
 for i in range(2000, 3201):
@@ -142,3 +163,57 @@ def digSum( n):
 # Driver method 
 n = 1234
 print (digSum(n)) 
+
+
+# 12 . two sum
+
+# Python program to find if there are 
+# two elements wtih given sum 
+  
+# function to check for the given sum 
+# in the array 
+def printPairs(arr, arr_size, sum): 
+      
+    # Create an empty hash set 
+    s = set() 
+      
+    for i in range(0, arr_size): 
+        temp = sum-arr[i] 
+        if (temp in s): 
+            print "Pair with given sum "+ str(sum) + " is (" + str(arr[i]) + ", " + str(temp) + ")"
+        s.add(arr[i]) 
+  
+# driver program to check the above function 
+A = [1, 4, 45, 6, 10, 8] 
+n = 16
+printPairs(A, len(A), n) 
+  
+    Output:
+
+
+Pair with given sum 16 is (10, 6)
+
+method 2:
+    
+    Two-pass Hash Table
+To improve our run time complexity, we need a more efficient way to check if the complement exists in the array. 
+If the complement exists, we need to look up its index. What is the best way to maintain a mapping of each element
+in the array to its index? A hash table.
+We reduce the look up time from O(n) to O(1) by trading space for speed. A hash table is built exactly for this purpose,
+it supports fast look up in near constant time. I say “near” because if a collision occurred, a look up could degenerate 
+to O(n) time. But look up in hash table should be amortized O(1) time as long as the hash function was chosen carefully.
+A simple implementation uses two iterations. In the first iteration, we add each element’s value and its index to the table.
+Then, in the second iteration we check if each element’s complement (target−nums[i]) exists in the table.
+Beware that the complement must not be nums[i]itself!
+hash_table={}
+for i in range(len(nums)):    # 先做一個hash table
+    hash_table[nums[i]]=i
+for i in range(len(nums)):
+    if target-nums[i] in hash_table:
+        if hash_table[target-nums[i]] != i:
+            return [i, hash_table[target-nums[i]]]
+return []
+
+
+
+
