531f8471fb60b1620fbeb78e120f13d183a3a8f5
doyager <nayanws@gmail.com>
2020-01-30 23:42:27 -0500

    Update pandas.py
    stats

--- python/pandas.py
+++ python/pandas.py
@@ -599,6 +599,37 @@ Name: VAR_new, dtype: object
 9  no_val        0
 
 
+##########################
+# summary statistics
+
+
+Live Demo
+import pandas as pd
+import numpy as np
+
+#Create a Dictionary of series
+d = {'Name':pd.Series(['Tom','James','Ricky','Vin','Steve','Smith','Jack',
+   'Lee','David','Gasper','Betina','Andres']),
+   'Age':pd.Series([25,26,25,23,30,29,23,34,40,30,51,46]),
+   'Rating':pd.Series([4.23,3.24,3.98,2.56,3.20,4.6,3.8,3.78,2.98,4.80,4.10,3.65])
+}
+
+#Create a DataFrame
+df = pd.DataFrame(d)
+print df.describe()
+Its output is as follows −
+
+               Age         Rating
+count    12.000000      12.000000
+mean     31.833333       3.743333
+std       9.232682       0.661628
+min      23.000000       2.560000
+25%      25.000000       3.230000
+50%      29.500000       3.790000
+75%      35.500000       4.132500
+max      51.000000       4.800000
+
+
 ###############
 #sum row values and sum col values
 df = pd.DataFrame([[4, 9],] * 3, columns=['A', 'B'])
