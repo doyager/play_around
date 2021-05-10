d25fa6bcd4a714f45a2b5a311dece2d43edf968b
doyager <nayanws@gmail.com>
2021-05-10 13:59:07 -0700

    Create virtual_env.py
    py virtual env

---
+++ python/virtual_env.py
@@ -0,0 +1,12 @@
+
+
+#to create virtual env and when you list on the current dir , you will see a folder with the name-of-env
+python3 -m venv <name-of-env>
+Eg : python3 -m venv venv
+  
+  
+#to activate [i..e. start using that virtual env]
+
+source venv/bin/activate
+
+
