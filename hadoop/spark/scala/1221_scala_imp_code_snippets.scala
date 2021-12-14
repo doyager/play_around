41cbc4b9c52f67db173e7989af5997f2fb89f0bc
doyager <nayanws@gmail.com>
2021-12-14 13:35:40 -0800

    Create 1221_scala_imp_code_snippets.scala
    misc

---
+++ hadoop/spark/scala/1221_scala_imp_code_snippets.scala
@@ -0,0 +1,20 @@
+
+
+//to add colum, filter based on conditions ( list of values for col , date time , hour , date ) value of 
+ sparkSession.udf.register("ensureTimestampInRange", HelperTools.standardizeToEpochMilliseconds _)
+.withColumn(
+        "eventSuccessFlag",
+        when($"eventType" isInCollection Const.EventMap("SUCCESS") and $"status" === "SUCCEEDED", 1)
+          .otherwise(0)
+      )
+      .withColumn(
+        "eventFailFlag",
+        when($"eventType" isInCollection Const.EventMap("FAIL") and $"status" === "FAILED", 1)
+          .otherwise(0)
+      )
+      .filter($"eventSuccessFlag" === 1 || $"eventFailFlag" === 1)
+      .filter($"ds" === date_add(lit(endDate.toString), 1))
+      .filter($"hr" <= hourPartition)
+      .filter(from_unixtime(expr("ensureTimestampInRange(createdAt)/1000.0")) >= startDate.toString + " 00:00:00")
+      .filter(from_unixtime(expr("ensureTimestampInRange(createdAt)/1000.0")) <= endDate.toString + " 23:59:59")
+      .withColumn("createdAtTs", from_unixtime(expr("ensureTimestampInRange(createdAt)/1000.0")))
