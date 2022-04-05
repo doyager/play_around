2752ccb3237ad793c30ed08c01a35845972ff501
doyager <nayanws@gmail.com>
2022-04-05 16:04:37 -0400

    Update misc_code_snippets.py
    remove dups pyspark

--- hadoop/spark/pyspark/misc_code_snippets.py
+++ hadoop/spark/pyspark/misc_code_snippets.py
@@ -87,3 +87,90 @@ df2.show()
 
 dframe.withColumn("c_number", dframe.col("c_a").cast("decimal(38,0)"))
 
+
+
+
+### Remove duplicates 
+
+schema = 'id int, name string'
+sampleDF = spark.createDataFrame(
+[[1,'Scott'], 
+[2,'Tiger'], 
+[3,'Jane'], 
+[4,'Jenny'], 
+[5,'Judy'],
+[3,'Jane'],
+[2,'Tiger']], schema=schema)
+
+# method - 1 : remove duplicates using distinct
+
+>>> newDF = sampleDF.distinct()
+>>> newDF.sort('id').show()
++---+-----+
+| id| name|
++---+-----+
+|  1|Scott|
+|  2|Tiger|
+|  3| Jane|
+|  4|Jenny|
+|  5| Judy|
++---+-----+
+
+# method - 2 : remove duplicates using dropDuplicates
+
+>>> newDF2 = sampleDF.dropDuplicates()
+>>> newDF2.sort('id').show()
++---+-----+
+| id| name|
++---+-----+
+|  1|Scott|
+|  2|Tiger|
+|  3| Jane|
+|  4|Jenny|
+|  5| Judy|
++---+-----+
+
+# method -3 : remove duplicates using  group by 
+
+The GROUP BY clause is used to group the rows based on a set of specified grouping columns and
+compute aggregations on the group of rows based on one or more specified aggregate function. 
+You can use groupBy to group duplicate rows using the count aggregate function.
+
+Consider following pyspark example remove duplicate from DataFrame using groupBy function.
+
+Pyspark:
+
+>>> newDF3 = sampleDF.groupBy("id", "name").count().select("id", "name").sort("id")
+>>> newDF3.show()
++---+-----+
+| id| name|
++---+-----+
+|  1|Scott|
+|  2|Tiger|
+|  3| Jane|
+|  4|Jenny|
+|  5| Judy|
++---+-----+
+
+# method - 4 : remove duplicates using row_number function
+
+Spark Window functions are used to calculate results such as the rank, row number etc over a range of input rows.
+The row_number() window function 
+returns a sequential number starting from 1 within a window partition.
+All duplicates values will have row number other then 1.
+
+>>> from pyspark.sql.window import Window
+>>> from pyspark.sql.functions import row_number
+
+>>> newDF4 = sampleDF.withColumn("row_number", row_number().over(Window.partitionBy("id", "name").orderBy("id"))).where("row_number = 1").sort("id").select("id", "name")
+>>> newDF4.show()
++---+-----+
+| id| name|
++---+-----+
+|  1|Scott|
+|  2|Tiger|
+|  3| Jane|
+|  4|Jenny|
+|  5| Judy|
++---+-----+
+
